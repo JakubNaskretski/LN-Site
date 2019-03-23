aaf106a02fdcc8c24b0f0046cc53cb89d5fa0129
Kuba Naskrętski <kuba2286@gmail.com>
2019-03-23 17:38:18 +0100

    added js function gotowe rozwiazania

--- static/appone/js/gotowerozwiazania.js
+++ static/appone/js/gotowerozwiazania.js
@@ -1,18 +1,11 @@
-
-// shows after click image
-// // Get the button, and when the user clicks on it, execute myFunction
-// document.getElementById("klikprodukt").onclick = function() {myFunction()};
-//
-// /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-// function myFunction() {
-//   document.getElementById("productphoto").classList.toggle("show");
-// }
-
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 window.onscroll = function() {
   scrollFunction1();
   scrollFunction2();
 }
+
+// calling function when click display image (for now alert)
+displayImageOnClick();
 
 function scrollFunction1() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -30,4 +23,11 @@ function scrollFunction2() {
     document.getElementById("imagebackground").setAttribute(
     "style","display: block; height: 100%; width: 100%; margin-left: auto; margin-right: auto; opacity: 0.5;");
   }
+}
+
+function displayImageOnClick() {
+    document.getElementById("btn1").addEventListener("click", function() {
+    alert("Hello World!");
+    }
+  );
 }
