b18548de94441be9a3d2f7291c5f8e2e13a23eb0
Kuba Naskrętski <kuba228@gmail.com>
2019-04-18 19:16:03 +0200

    anned secretkey profile

--- LNSITE/settings.py
+++ LNSITE/settings.py
@@ -17,18 +17,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'products')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')8s=@r^h136%fy#r8l@40agj1a!o9mt_0zl++89r2t_wq2-w7&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.24', '127.0.0.1', '192.168.1.22', '192.168.3.73']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'ln-site.herokuapp.com']
 
 
 # Application definition
