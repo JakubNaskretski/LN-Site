ff08662296cd8691003e8ef83fc58e480a399a3b
Kuba Naskrętski <kuba228@gmail.com>
2019-04-19 08:48:37 +0200

    Make it better!

--- LNSITE/settings.py
+++ LNSITE/settings.py
@@ -15,9 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'products')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_APPONE = os.path.join(STATIC_ROOT, 'appone')
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+STATICFILES_DIRS = [STATIC_ROOT,]
 MEDIA_URL = '/products/'
 django_heroku.settings(locals())
