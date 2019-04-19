45b93b762539e4ed10ce9f18ae6e46c2736a155e
Kuba Naskrętski <kuba228@gmail.com>
2019-04-19 08:50:19 +0200

    Make it better!

--- LNSITE/settings.py
+++ LNSITE/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'products')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_APPONE = os.path.join(STATIC_ROOT, 'appone')
 
 
