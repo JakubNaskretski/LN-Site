86abed38b6fec3863243a3dc9c74bd08e7bd3f6c
Kuba Naskrętski <kuba228@gmail.com>
2019-04-19 08:57:40 +0200

    Make it better!

--- LNSITE/settings.py
+++ LNSITE/settings.py
@@ -13,7 +13,7 @@ import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'products')
